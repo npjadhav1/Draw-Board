50486d3c73ee715663e90fe9fc6920d8c56334fd
Nikita Jadhav <npjadhav1011@gmail.com>
2025-02-10 03:19:18 +0530

    completed

---
+++ script.js
@@ -0,0 +1,43 @@
+// const board = document.getElementById("board");
+
+// board.addEventListener("mouseenter", () => {
+//     board.style.backgroundColor = "blue";
+// });
+const boxes = document.querySelectorAll(".child"); // 64 --> array
+const eraserBtn = document.getElementById("eraser-button");
+const input = document.getElementById("my-input");
+
+let color = "black";
+
+// input.onchange = () => {
+//   color = input.value;
+// };
+
+eraserBtn.onclick = () => {
+  color = "white";
+};
+
+// onmousedown
+let isMouseClicked = false;
+
+document.onmousedown = () => {
+  isMouseClicked = true;
+};
+
+// onmouseup
+document.onmouseup = () => {
+  isMouseClicked = false;
+};
+
+for (let i = 0; i <= 64; i++) {
+  boxes[i].onmouseenter = () => {
+    if (isMouseClicked === true) {
+      boxes[i].style.backgroundColor = color;
+    }
+  };
+}
+
+function setColor() {
+  const userChoice = prompt("Enter color");
+  color = userChoice;
+}
